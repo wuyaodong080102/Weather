61aa49808bc6ea96c175cee3a8b489bd6b305f95
Wu Yaodong <qianfeng@yuwancumian.local>
2016-11-02 10:01:09 +0800

    Initial Commit

---
+++ WeatherRemind/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherRemind
+//
+//  Created by MrWu on 2016/11/2.
+//  Copyright © 2016年 WYD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
